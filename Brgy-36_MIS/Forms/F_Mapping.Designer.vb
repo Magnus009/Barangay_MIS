8f2a0405c06c33b1e7f1ccaf2ee7ce552f13aada
Fred <fred36060@gmail.com>
2021-05-12 15:16:53 +0800

    2021.05.12

---
+++ Brgy-36_MIS/Forms/F_Mapping.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class F_Mapping
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picBox = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.picBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picBox
+        '
+        Me.picBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.picBox.Location = New System.Drawing.Point(73, 32)
+        Me.picBox.Name = "picBox"
+        Me.picBox.Size = New System.Drawing.Size(100, 50)
+        Me.picBox.TabIndex = 0
+        Me.picBox.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(436, 19)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'F_Mapping
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(992, 690)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.picBox)
+        Me.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "F_Mapping"
+        Me.Text = "F_Mapping"
+        CType(Me.picBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents picBox As System.Windows.Forms.PictureBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+End Class
---
+++ Brgy-36_MIS/Forms/F_Mapping.vb
@@ -0,0 +1,38 @@
+﻿Public Class F_Mapping
+
+    Private initPoint As System.Nullable(Of Point) = Nothing
+    'Private g As Graphics = CreateGraphics()
+
+
+    Dim intH As Integer
+    Dim intW As Integer
+    Private Sub F_Mapping_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        initPoint = e.Location
+        F_Mapping_MouseMove(sender, e)
+    End Sub
+
+    Private Sub F_Mapping_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        If initPoint IsNot Nothing Then
+            Using g As Graphics = CreateGraphics()
+                intH = initPoint.Value.Y - e.Location.Y
+                intW = initPoint.Value.X - e.Location.X
+                intH = IIf(intH < 0, intH * -1, intH)
+                intW = IIf(intW < 0, intW * -1, intW)
+
+                TextBox1.Text = intH.ToString + "x" + intW.ToString
+                g.DrawRectangle(Pens.Black, initPoint.Value.X, initPoint.Value.Y, intW, intH)
+                g.Dispose()
+            End Using
+            picBox.Invalidate()
+            Me.Refresh()
+        End If
+    End Sub
+
+    Private Sub F_Mapping_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+        Using g As Graphics = CreateGraphics()
+            g.DrawRectangle(Pens.Black, initPoint.Value.X, initPoint.Value.Y, intW, intH)
+        End Using
+        picBox.Invalidate()
+        initPoint = Nothing
+    End Sub
+End Class
