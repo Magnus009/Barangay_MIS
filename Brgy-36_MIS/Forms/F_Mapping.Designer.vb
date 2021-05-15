203858b12055d474955f63ee9658cccf9461ac77
Fred <fred36060@gmail.com>
2021-05-15 15:20:26 +0800

    2021.05.15

--- Brgy-36_MIS/Forms/F_Mapping.Designer.vb
+++ Brgy-36_MIS/Forms/F_Mapping.Designer.vb
@@ -23,32 +23,23 @@ Partial Class F_Mapping
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.picBox = New System.Windows.Forms.PictureBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         CType(Me.picBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'picBox
         '
         Me.picBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.picBox.Location = New System.Drawing.Point(73, 32)
+        Me.picBox.Location = New System.Drawing.Point(0, 0)
         Me.picBox.Name = "picBox"
         Me.picBox.Size = New System.Drawing.Size(100, 50)
         Me.picBox.TabIndex = 0
         Me.picBox.TabStop = False
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(436, 19)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
-        Me.TextBox1.TabIndex = 1
         '
         'F_Mapping
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(992, 690)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.picBox)
         Me.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
@@ -56,9 +47,7 @@ Partial Class F_Mapping
         Me.Text = "F_Mapping"
         CType(Me.picBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents picBox As System.Windows.Forms.PictureBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 End Class
--- Brgy-36_MIS/Forms/F_Mapping.vb
+++ Brgy-36_MIS/Forms/F_Mapping.vb
@@ -1,38 +1,74 @@
 ﻿Public Class F_Mapping
-
     Private initPoint As System.Nullable(Of Point) = Nothing
-    'Private g As Graphics = CreateGraphics()
+    Private lowestPoint As System.Nullable(Of Point) = Nothing
+    Private highestPoint As System.Nullable(Of Point) = Nothing
 
-
-    Dim intH As Integer
-    Dim intW As Integer
+    Dim intH, intW As Integer
+    Dim intY, intX As Integer
     Private Sub F_Mapping_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+
         initPoint = e.Location
+        highestPoint = e.Location
+        lowestPoint = e.Location
         F_Mapping_MouseMove(sender, e)
     End Sub
 
     Private Sub F_Mapping_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
         If initPoint IsNot Nothing Then
             Using g As Graphics = CreateGraphics()
+                Dim rect As New Rectangle(intX, intY, intW, intH)
+
                 intH = initPoint.Value.Y - e.Location.Y
                 intW = initPoint.Value.X - e.Location.X
+
+                If e.Location.Y < initPoint.Value.Y Then
+                    intY = e.Location.Y
+                Else
+                    intY = initPoint.Value.Y
+                End If
+
+                If e.Location.X < initPoint.Value.X Then
+                    intX = e.Location.X
+                Else
+                    intX = initPoint.Value.X
+                End If
+
+
+
                 intH = IIf(intH < 0, intH * -1, intH)
                 intW = IIf(intW < 0, intW * -1, intW)
 
-                TextBox1.Text = intH.ToString + "x" + intW.ToString
-                g.DrawRectangle(Pens.Black, initPoint.Value.X, initPoint.Value.Y, intW, intH)
-                g.Dispose()
+                g.DrawRectangle(New Pen(Brushes.SkyBlue, 2), rect)
+                'g.DrawRectangle(New Pen(Brushes.SkyBlue, 2), intX, intY, intW, intH)
+                If e.Location.X > initPoint.Value.X Or e.Location.Y > initPoint.Value.Y Then
+                    If (e.Location.X > highestPoint.Value.X) Or (e.Location.Y > highestPoint.Value.Y) Then
+                        highestPoint = e.Location
+                    Else
+                        g.Dispose()
+                        Me.Refresh()
+                        highestPoint = e.Location
+                    End If
+                Else
+                    If (e.Location.X < lowestPoint.Value.X) Or (e.Location.Y < lowestPoint.Value.Y) Then
+                        lowestPoint = e.Location
+                    Else
+                        g.Dispose()
+                        Me.Refresh()
+                        lowestPoint = e.Location
+                    End If
+                End If
             End Using
             picBox.Invalidate()
-            Me.Refresh()
         End If
     End Sub
 
     Private Sub F_Mapping_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
         Using g As Graphics = CreateGraphics()
-            g.DrawRectangle(Pens.Black, initPoint.Value.X, initPoint.Value.Y, intW, intH)
+            g.DrawRectangle(New Pen(Brushes.Black, 2), intX, intY, intW, intH)
         End Using
         picBox.Invalidate()
         initPoint = Nothing
+        highestPoint = Nothing
+        lowestPoint = Nothing
     End Sub
 End Class
